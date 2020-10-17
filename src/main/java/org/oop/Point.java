93906968a4c7c0a7723e75aa86037fe6c4c902ac
Gourav <gourav2824@gmail.com>
2020-10-17 22:20:35 +0530

    my oo solution

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,20 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distance(Point point){
+        double xDistance = point.x - this.x;
+        double yDistance = point.y - this.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+
+    public double direction(Point point){
+        double xDistance = point.x - this.x;
+        double yDistance = point.y - this.y;
+        return Math.atan2(yDistance, xDistance);
     }
 }
